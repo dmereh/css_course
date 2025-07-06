5bcbbfb0a797fd5109ecdc7a0801461cb8e6c4e0
dmereh <deemereh@gmail.com>
2025-07-06 02:03:02 +0100

    new changes & js

---
+++ javascript/variables.js
@@ -0,0 +1,22 @@
+// var firstName = "deeqa" //string
+// var lastName = "mereh" //string
+// var age = 36 //number
+// var isMarried = false //boolean
+// var hobbies = ["reading", "swimming", "coding"] //array
+// var occupation = "software engineer" //string
+// var country = "united kingdom"
+// var city = "london"
+// var address = "123 main street" //object   
+// var postalCode = "abc123"
+// var phoneNumber = "1234567890"
+// var salary = 50000;
+// var taxRate = 0.2;  
+
+
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase());
+// console.log(salary * taxRate);
+
+//  console.log(typeof firstName)
+
+
