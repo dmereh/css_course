fd51111caa9ebcbdde898bf9650131f09e6b0620
dmereh <deemereh@gmail.com>
2025-07-21 01:12:50 +0100

    strings_cont_and_arrays

---
+++ javascript/arrays/arrays.js
@@ -0,0 +1,24 @@
+let students = ["Mahmoud","Hannan","Abdulkadir","Deeqa"] //create list string
+let studentAges = [30,25,27,28] //create list number
+
+
+// console.log(studentAges)
+// console.log(students)
+// console.log(students[0])
+// students[2] = "Fatoma" //replace list using index
+// students.push("Abdulghani") //add to the end of the array string
+// studentAges.push(73) //add to the array number
+// students.pop("Deeqa") //remove from the end of the array
+// students.shift("Mahmoud") //remove from the beginning of the array
+// students.unshift("Fatoma")
+// students.splice(1, 0, "Fatoma"); //inserts at index 1 0 = do not replace
+// students.splice(4, 1, "Idil") //inserts at index 4, 1 = replace the index
+// let smallStudents = students.slice(0, 3) //slice from index 0 to 2
+// console.log(smallStudents)
+
+
+
+console.log(students)
+console.log(students.sort()) //sort the list alphabetically
+console.log(students.reverse()) // sort the list in reverse order
+
--- javascript/strings/string.js
+++ javascript/strings/string.js
@@ -1,2 +1,41 @@
-const fullName = "Deeqa Mereh"
-console.log(fullName.toUpperCase())
+// let town = "London"
+
+// const fullName1 = "Deeqa Mereh" //double quotes
+// const fullName2 = 'Deeqa Mereh' //single quotes
+// const fullName3 = `Deeqa Mereh`; //template literals using back ticks
+// const fullName4 = new String ("Deeqa Mereh"); //string object using the new keyword
+
+// // let fullSentence = "Hello, my name is " + fullName1 + " and I am from " + town; //concatenation
+// let fullSentence = `Hello, my name is ${fullName1} and I am from ${town}
+// console.log(fullSentence)
+
+// // console.log(fullName.toUpperCase())
+
+// let fullName = "Deeqa Abdulghani Ahmed Mereh";
+// console.log(fullName.length); // how many characters in the value
+// console.log(fullName.toUpperCase()); // convert to uppercase
+// console.log(fullName.toLowerCase()); // convert to lowercase
+// console.log(fullName.charAt(0)); // get the first character
+// console.log(fullName.indexOf("a")); // get the index of the first occurrence of the character
+// console.log(fullName.slice(0, 5)); // get a part of the string
+// console.log(fullName.split("")); // split the string into an array of characters
+// console.log(fullName.replace("Abdulghani", "A")); // replace a part of the string
+// console.log(fullName.trim()); // remove leading and trailing spaces
+// console.log(fullName.includes("a")); // check if the string includes a certain character
+// console.log(fullName.startsWith("De")); // check if the string starts with a certain character
+// console.log(fullName.endsWith("h")); // check if the string ends with a certain character
+// console.log(fullName.repeat(2)); // repeat the string
+// console.log(fullName.padStart(20, "*")); // add spaces to the left of the string
+// console.log(fullName.padEnd(20, "*")); // add spaces to the right of the string
+// console.log(fullName.concat(" ").concat(town)) // add another value to the string
+// console.log(fullName + " " + town) // add another value to the string - shorter version
+
+let ukNationalAnthem =
+  "God save our gracious King!\nLong live our noble King!\nGod save the King!\nSend him victorious,\nHappy and glorious,\nLong to reign over us,\nGod save the King!";
+// console.log(ukNationalAnthem[2]) // [] used to find index
+
+if (ukNationalAnthem.startsWith("King")) {
+  console.log("The UK national anthem starts with the word 'King'");
+} else {
+  console.log("The UK national anthem does not start with the word 'King'");
+}
