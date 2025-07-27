4f33196a695c06b496b88f244f99927f52b3d282
dmereh <deemereh@gmail.com>
2025-07-27 16:53:49 +0100

    arrow functions

---
+++ javascript/arrowFunctions/arrow_functions.js
@@ -0,0 +1,25 @@
+// function multiply(){
+//     return 2 + 2
+// }
+
+// let result = multiply()
+// console.log(result)
+
+// function(){ 
+//     result 2 + 2
+// } //anonymous function - a valid function without a name
+
+// let multiply = () => {
+//     return 2 + 2
+// } //arrow function - a shorthand for a function
+
+
+// let multiply = (num1) => num1 + 2 // arrow function - another way to do shorthand
+
+// let result = multiply(3)
+// console.log(result)
+
+
+let multiply = (num1, num2) => num1 + num2 // arrow function - another way to
+let result = multiply(3, 4)
+console.log(result) 
---
+++ javascript/strictMode/strict_mode.js
@@ -0,0 +1,5 @@
+"use strict"
+let firstName = "Deeqa"
+console.log(firstName)
+
+//use strict ensures that the code is run in strict mode meaning it cannot be run without variables (e.g. var, let, const)
