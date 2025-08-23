4f36f0db032462f220cf51d46452ec1bad461a75
dmereh <deemereh@gmail.com>
2025-08-23 21:18:04 +0100

    updates

--- react/chapter_1_intro/my-react-app/src/App.jsx
+++ react/chapter_1_intro/my-react-app/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// export default function App() {
+//   return <h1> Deeqa Mereh </h1>;
+// }
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Fragment } from "react";
 
+const name = "Deeqa";
+const year = new Date().getFullYear();
+export default function App() {
+  const items = [{name:"Ali", email: "ali@gmail.com"}, {name: "Mohamed", email: "Mohamed@gmail.com"}, {name: "Hassan", email: "hassan@gmail.com"}];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome, {name}!</h1>
+      <p>Year: {year}</p>
+      <ul>
+        {items.map((item) => (
+          <li key={item.name}>{item.name} the email is {item.email}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
